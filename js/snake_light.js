b0f7a3bac21ed61a0e1da2226ba1527c44f4dad0
Micah <micahbello@gmail.com>
2018-06-01 17:45:22 -0400

    Fix css and add tab icon

--- js/snake_light.js
+++ js/snake_light.js
@@ -64,12 +64,165 @@
 /******/ })
 /************************************************************************/
 /******/ ([
-/* 0 */,
+/* 0 */
+/***/ (function(module, exports) {
+
+class DOMNodeCollection {
+  constructor(elements){ //should only receive an array
+    this.elements = elements
+  }
+
+
+  html(arg) {
+    if (!arg) {
+      return this.elements[0].textContent;
+    } else {
+      this.elements.forEach(node => {
+        node.innerHTML = arg
+      });
+    }
+  }
+
+  empty() {
+    this.elements.forEach(node => {
+      node.innerHTML = "";
+    });
+  }
+
+  append(arg) {
+    this.elements.forEach(node => {
+      node.innerHTML = node.innerHTML + arg;
+    });
+  }
+
+
+  children() {
+    let childrenArray = [];
+    this.elements.forEach(node => {
+      for (let i = 0; i < node.children.length; i++) {
+        childrenArray.push(node.children[i])
+      }
+    });
+    return new DOMNodeCollection(childrenArray);
+  }
+
+  parent() {
+    let parentsArray = [];
+
+    this.elements.forEach(node => {
+      parentsArray.push(node.parentNode);
+    });
+    return new DOMNodeCollection(parentsArray);
+  }
+
+  find(arg) {
+    let matchingDescendants = [];
+
+    this.elements.forEach(node => {
+      matchingDescendants.push(node.querySelectorAll(arg));
+    });
+
+    let matchingDescendantsArray = [];
+
+    for(let i=0; i < matchingDescendants.length; i++) {
+      for (let j=0; j < matchingDescendants[i].length; j++) {
+        matchingDescendantsArray.push(matchingDescendants[i][j]);
+      }
+    }
+    return new DOMNodeCollection(matchingDescendantsArray);
+  }
+
+  remove(arg) {
+
+   this.elements.forEach(node => {
+    if (!arg){
+      node.remove();
+      } else {
+        arg === "." + node.className ? node.remove() : null
+      }
+   });
+  }
+
+  on(e, callback) {
+    this.elements.forEach(node => {
+      node.addEventListener(e, callback)
+      node.callback = callback;
+    });
+  }
+
+  off(e) {
+    this.elements.forEach(node => {
+      node.removeEventListener(e, node.callback);
+    });
+  }
+
+  attr(attribute, value) {
+
+      if (!value) {
+        if (this.elements[0].attributes[attribute] === undefined) {
+          return undefined
+        } else {
+        return this.elements[0].attributes[attribute].nodeValue
+        }
+
+      } else {
+        this.elements.forEach((element) => {
+          if (element.attributes[attribute]) {
+            element.attributes[attribute].nodeValue= value;
+          } else {
+            element.setAttribute(attribute, value);
+          }
+        });
+      }
+  }
+
+  addClass(classNames) {
+    this.elements.forEach(element => {
+      if (element.classList.length === 0) {
+      element.setAttribute("class", classNames)
+      } else {
+        let classNamesSplit = classNames.split(" ");
+        for(let i = 0; i < classNamesSplit.length; i++) {
+          element.classList.add(classNamesSplit[i]);
+        }
+
+      }
+    });
+  }
+  //
+  removeClass(classNames) {
+    if (!classNames) {
+      this.elements.forEach(element => {
+        element.setAttribute("class", "");
+      });
+    } else {
+      let classNamesSplit = classNames.split(" ");
+      this.elements.forEach(element => {
+        let classListLength = element.classList.length;
+
+        for(let i = 0; i < classListLength; i++) {
+          for (let j = 0; j < classNamesSplit.length; j++) {
+            if (element.classList[i] === classNamesSplit[j]) {
+              element.classList.remove(classNamesSplit[j]);
+            }
+          }
+        }
+      });
+    }
+  }
+
+}
+
+module.exports = DOMNodeCollection;
+// export default DOMNodeCollection;
+
+
+/***/ }),
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const DOMNodeCollection = __webpack_require__(9);
-const main_functions = __webpack_require__(10);
+const DOMNodeCollection = __webpack_require__(0);
+const main_functions = __webpack_require__(2);
 const GameView = __webpack_require__(3);
 
 
@@ -79,7 +232,53 @@ const GameView = __webpack_require__(3);
 
 
 /***/ }),
-/* 2 */,
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const DOMNodeCollection = __webpack_require__(0);
+// import DOMNodeCollection from './dom_node_collection.js';
+//
+function $l(selector) {
+
+  if(document.readyState === "complete" && typeof selector === "function") {
+    selector();
+  }
+
+  let queue = [];
+
+  if (typeof selector === "string") {
+    let nodelist = document.querySelectorAll(selector);
+    nodelistArray = Array.from(nodelist);
+    return new DOMNodeCollection(nodelistArray)
+  } else if (typeof selector === "function") {
+    queue.push(selector)
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    for(let i = 0; i < queue.length; i++){
+      queue[i]();
+    }
+  });
+}
+
+  $l.extend = (...args) => {
+    return Object.assign(...args);
+  }
+
+  $l.ajax = (optionsObject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(optionsObject.type, optionsObject.url);
+    xhr.send();
+    xhr.onload = function () {
+    console.log(xhr.status);
+    console.log(JSON.parse(xhr.response));
+    }
+  }
+
+window.$l = $l
+
+
+/***/ }),
 /* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -226,7 +425,7 @@ module.exports = GameView;
 /***/ (function(module, exports, __webpack_require__) {
 
 const Snake = __webpack_require__(5);
-const Apple = __webpack_require__(8);
+const Apple = __webpack_require__(6);
 
 class Board {
   constructor() {
@@ -444,9 +643,7 @@ module.exports = Snake;
 
 
 /***/ }),
-/* 6 */,
-/* 7 */,
-/* 8 */
+/* 6 */
 /***/ (function(module, exports) {
 
 class Apple {
@@ -456,207 +653,6 @@ class Apple {
 }
 
 module.exports = Apple;
-
-
-/***/ }),
-/* 9 */
-/***/ (function(module, exports) {
-
-class DOMNodeCollection {
-  constructor(elements){ //should only receive an array
-    this.elements = elements
-  }
-
-
-  html(arg) {
-    if (!arg) {
-      return this.elements[0].textContent;
-    } else {
-      this.elements.forEach(node => {
-        node.innerHTML = arg
-      });
-    }
-  }
-
-  empty() {
-    this.elements.forEach(node => {
-      node.innerHTML = "";
-    });
-  }
-
-  append(arg) {
-    this.elements.forEach(node => {
-      node.innerHTML = node.innerHTML + arg;
-    });
-  }
-
-
-  children() {
-    let childrenArray = [];
-    this.elements.forEach(node => {
-      for (let i = 0; i < node.children.length; i++) {
-        childrenArray.push(node.children[i])
-      }
-    });
-    return new DOMNodeCollection(childrenArray);
-  }
-
-  parent() {
-    let parentsArray = [];
-
-    this.elements.forEach(node => {
-      parentsArray.push(node.parentNode);
-    });
-    return new DOMNodeCollection(parentsArray);
-  }
-
-  find(arg) {
-    let matchingDescendants = [];
-
-    this.elements.forEach(node => {
-      matchingDescendants.push(node.querySelectorAll(arg));
-    });
-
-    let matchingDescendantsArray = [];
-
-    for(let i=0; i < matchingDescendants.length; i++) {
-      for (let j=0; j < matchingDescendants[i].length; j++) {
-        matchingDescendantsArray.push(matchingDescendants[i][j]);
-      }
-    }
-    return new DOMNodeCollection(matchingDescendantsArray);
-  }
-
-  remove(arg) {
-
-   this.elements.forEach(node => {
-    if (!arg){
-      node.remove();
-      } else {
-        arg === "." + node.className ? node.remove() : null
-      }
-   });
-  }
-
-  on(e, callback) {
-    this.elements.forEach(node => {
-      node.addEventListener(e, callback)
-      node.callback = callback;
-    });
-  }
-
-  off(e) {
-    this.elements.forEach(node => {
-      node.removeEventListener(e, node.callback);
-    });
-  }
-
-  attr(attribute, value) {
-
-      if (!value) {
-        if (this.elements[0].attributes[attribute] === undefined) {
-          return undefined
-        } else {
-        return this.elements[0].attributes[attribute].nodeValue
-        }
-
-      } else {
-        this.elements.forEach((element) => {
-          if (element.attributes[attribute]) {
-            element.attributes[attribute].nodeValue= value;
-          } else {
-            element.setAttribute(attribute, value);
-          }
-        });
-      }
-  }
-
-  addClass(classNames) {
-    this.elements.forEach(element => {
-      if (element.classList.length === 0) {
-      element.setAttribute("class", classNames)
-      } else {
-        let classNamesSplit = classNames.split(" ");
-        for(let i = 0; i < classNamesSplit.length; i++) {
-          element.classList.add(classNamesSplit[i]);
-        }
-
-      }
-    });
-  }
-  //
-  removeClass(classNames) {
-    if (!classNames) {
-      this.elements.forEach(element => {
-        element.setAttribute("class", "");
-      });
-    } else {
-      let classNamesSplit = classNames.split(" ");
-      this.elements.forEach(element => {
-        let classListLength = element.classList.length;
-
-        for(let i = 0; i < classListLength; i++) {
-          for (let j = 0; j < classNamesSplit.length; j++) {
-            if (element.classList[i] === classNamesSplit[j]) {
-              element.classList.remove(classNamesSplit[j]);
-            }
-          }
-        }
-      });
-    }
-  }
-
-}
-
-module.exports = DOMNodeCollection;
-// export default DOMNodeCollection;
-
-
-/***/ }),
-/* 10 */
-/***/ (function(module, exports, __webpack_require__) {
-
-const DOMNodeCollection = __webpack_require__(9);
-// import DOMNodeCollection from './dom_node_collection.js';
-//
-function $l(selector) {
-
-  if(document.readyState === "complete" && typeof selector === "function") {
-    selector();
-  }
-
-  let queue = [];
-
-  if (typeof selector === "string") {
-    let nodelist = document.querySelectorAll(selector);
-    nodelistArray = Array.from(nodelist);
-    return new DOMNodeCollection(nodelistArray)
-  } else if (typeof selector === "function") {
-    queue.push(selector)
-  }
-
-  document.addEventListener("DOMContentLoaded", () => {
-    for(let i = 0; i < queue.length; i++){
-      queue[i]();
-    }
-  });
-}
-
-  $l.extend = (...args) => {
-    return Object.assign(...args);
-  }
-
-  $l.ajax = (optionsObject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(optionsObject.type, optionsObject.url);
-    xhr.send();
-    xhr.onload = function () {
-    console.log(xhr.status);
-    console.log(JSON.parse(xhr.response));
-    }
-  }
-
-window.$l = $l
 
 
 /***/ })
