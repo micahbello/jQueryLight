4e4dc642fe35c6345fa4ac98e4fb470ac3bc4943
Micah <micahbello@gmail.com>
2018-06-21 13:29:32 -0400

    Prepare to refactor snake collision detection method

--- bundle.js
+++ bundle.js
@@ -611,93 +611,98 @@ class Board {
   }
 
 
-//   willSnakeCollide() {
-//     const newSegments = nextSnakeSegments();
-//     // const currentDirection = this.snake.direction;
-//     const nextHeadPos = (currentHeadPos) => {
 //
-//       //first check if the head will collide anywhere on the body of the the snake
-//
-//
-//
-//       if (this.snake.direction === "up") {
-//
-//       } else if (this.snake.direction === "down") {
-//
-//       } else if (this.snake )
-//
-//     }
-//   }
+  willSnakeCollide() {
+    const newSegments = nextSnakeSegments();
+    // const currentDirection = this.snake.direction;
+
+    let colliding = false;
+
+    if (newSegments[0][0] < 0 || newSegments[0][0] > 19
+      || newSegments[0][1] < 0 || newSegments[0][1] > 19) {
+      colliding = true;
+    } else if (newSegments.length> 1){
+      let snakeSegments = newSegments.slice(1); // all segments except head
+      let snakeHead = newSegments[0];
+
+      snakeSegments.forEach(segment => {
+        if (segment[0] === snakeHead[0] && segment[1] === snakeHead[1]) {
+          colliding = true;
+        }
+      });
+    }
+    return colliding;
+  }
 //
 // // /*
 // //use the move method from snake as reference
 //
-//   nextSnakeSegments() {
-//
-//     let newSegments = [];
-//     //the ifs correspond to the direction of the head, that determines what the head passes to the
-//     //next link which takes its place after every render
-//     if (this.snake.direction === "up") {
-//       for (let i = 0; i < this.snake.segments.length; i ++) {
-//         if (i === 0) {
-//           let coord1 = this.snake.segments[i][0];   //the x axix
-//           let coord2 = this.snake.segments[i][1] - 1;  //the y axis
-//
-//           //this "newSegment" becomes the new head
-//           let newSegment = [coord1, coord2, "up", "head"];
-//           //gets placed in a newSegment array as the first one
-//           newSegments[i] = newSegment;
-//         } else {
-//           newSegments[i] = this.snake.segments[i - 1];
-//           newSegments[i][3] = "body"
-//         }
-//       }
-//     } else if (this.snake.direction === "down") {
-//       for (let i = 0; i < this.snake.segments.length; i ++) {
-//         if (i === 0) {
-//           let coord1 = this.snake.segments[i][0];
-//           let coord2 = this.snake.segments[i][1] + 1;
-//
-//           let newSegment = [coord1, coord2, "down", "head"];
-//           newSegments[i] = newSegment;
-//         } else {
-//           newSegments[i] = this.snake.segments[i - 1];
-//           newSegments[i][3] = "body";
-//         }
-//       }
-//     } else if (this.snake.direction === "left") {
-//       for (let i = 0; i < this.snake.segments.length; i ++) {
-//         if (i === 0) {
-//           let coord1 = this.snake.segments[i][0] - 1;
-//           let coord2 = this.snake.segments[i][1];
-//
-//           let newSegment = [coord1, coord2, "left", "head"];
-//           newSegments[i] = newSegment;
-//         } else {
-//           newSegments[i] = this.snake.segments[i - 1];
-//           newSegments[i][3] = "body"
-//         }
-//       }
-//     } else if (this.snake.direction === "right") {
-//
-//       for (let i = 0; i < this.snake.segments.length; i ++) {
-//         if (i === 0) {
-//           let coord1 = this.snake.segments[i][0] + 1;
-//           let coord2 = this.snake.segments[i][1];
-//
-//           let newSegment = [coord1, coord2, "right", "head"];
-//           newSegments[i] = newSegment;
-//         } else {
-//           newSegments[i] = this.snake.segments[i - 1];
-//           newSegments[i][3] = "body"
-//         }
-//       }
-//     }
-//
-//     return newSegments;
-//   }
-//   // */
+  nextSnakeSegments() {
 
+    let newSegments = [];
+    //the ifs correspond to the direction of the head, that determines what the head passes to the
+    //next link which takes its place after every render
+    if (this.snake.direction === "up") {
+      for (let i = 0; i < this.snake.segments.length; i ++) {
+        if (i === 0) {
+          let coord1 = this.snake.segments[i][0];   //the x axix
+          let coord2 = this.snake.segments[i][1] - 1;  //the y axis
+
+          //this "newSegment" becomes the new head
+          let newSegment = [coord1, coord2, "up", "head"];
+          //gets placed in a newSegment array as the first one
+          newSegments[i] = newSegment;
+        } else {
+          newSegments[i] = this.snake.segments[i - 1];
+          newSegments[i][3] = "body"
+        }
+      }
+    } else if (this.snake.direction === "down") {
+      for (let i = 0; i < this.snake.segments.length; i ++) {
+        if (i === 0) {
+          let coord1 = this.snake.segments[i][0];
+          let coord2 = this.snake.segments[i][1] + 1;
+
+          let newSegment = [coord1, coord2, "down", "head"];
+          newSegments[i] = newSegment;
+        } else {
+          newSegments[i] = this.snake.segments[i - 1];
+          newSegments[i][3] = "body";
+        }
+      }
+    } else if (this.snake.direction === "left") {
+      for (let i = 0; i < this.snake.segments.length; i ++) {
+        if (i === 0) {
+          let coord1 = this.snake.segments[i][0] - 1;
+          let coord2 = this.snake.segments[i][1];
+
+          let newSegment = [coord1, coord2, "left", "head"];
+          newSegments[i] = newSegment;
+        } else {
+          newSegments[i] = this.snake.segments[i - 1];
+          newSegments[i][3] = "body"
+        }
+      }
+    } else if (this.snake.direction === "right") {
+
+      for (let i = 0; i < this.snake.segments.length; i ++) {
+        if (i === 0) {
+          let coord1 = this.snake.segments[i][0] + 1;
+          let coord2 = this.snake.segments[i][1];
+
+          let newSegment = [coord1, coord2, "right", "head"];
+          newSegments[i] = newSegment;
+        } else {
+          newSegments[i] = this.snake.segments[i - 1];
+          newSegments[i][3] = "body"
+        }
+      }
+    }
+
+    return newSegments;
+  }
+//   // */
+//
 
   loosingCollisions() {
 
